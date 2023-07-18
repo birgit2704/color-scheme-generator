c1e71a2907368690f497f8f7514c07bf0bcefa09
Birgit <birgit.bartenschlager@web.de>
2023-07-18 18:00:57 +0200

    html + css

---
+++ index.js
@@ -0,0 +1,7 @@
+// <!-- https://www.thecolorapi.com/docs#schemes -->
+// <!-- Requirements:
+// - choose the "seed color" with an <input type = color /
+// - choose the color scheme mode in an select box
+// - clicking button makes request the the color api to get a color scheme
+// - display the scheme colors and hex values on the page
+// - click hex values/colors to copy to clipboard -->
