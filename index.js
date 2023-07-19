841ce5a3ee536f86159439afc12eb40273e76180
Birgit <birgit.bartenschlager@web.de>
2023-07-19 07:57:28 +0200

    show selected color code

--- index.js
+++ index.js
@@ -14,26 +14,29 @@ document.getElementById("btn").addEventListener("click", () => {
 
   fetch(`${getQueryUrl()}`)
     .then((res) => res.json())
-    .then((data) => {
-      const colorArray = data.colors.map((el) => el.hex.value);
-      const colorHtml = colorArray
-        .map(
-          (color) => `
-                <div class="color-container">
-                    <div class="color" style="background-color: ${color}"></div>
-                    <div class="hex-code">${color}</div>
-                </div>
-                `
-        )
-        .join("");
-      document.getElementById("color-container").innerHTML = colorHtml;
-    });
+    .then((data) => renderColorScheme(data));
 });
 
 function getQueryUrl() {
   const seedColor = document.querySelector("input").value.slice(1);
   const scheme = document.querySelector("select").value;
+  document.getElementById("seed-color-hex").textContent = seedColor;
   return `https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${scheme}&count=5`;
+}
+
+function renderColorScheme(data) {
+  const colorArray = data.colors.map((el) => el.hex.value);
+  const colorHtml = colorArray
+    .map(
+      (color) => `
+                <div class="color-container">
+                    <div class="color" style="background-color: ${color}"></div>
+                    <div class="hex-code">${color}</div>
+                </div>
+                `
+    )
+    .join("");
+  document.getElementById("color-container").innerHTML = colorHtml;
 }
 
 // <!-- https://www.thecolorapi.com/docs#schemes -->
