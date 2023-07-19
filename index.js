228fd958123badf501b0b81b2783725f05eecb64
Birgit <birgit.bartenschlager@web.de>
2023-07-19 10:33:14 +0200

    refactor code

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 import schemes from "./schemes.js";
 
-document
-  .getElementById("colors-container")
-  .addEventListener("click", function (e) {
-    copyTextToClipboard(e);
-  });
+const colorsContainerEl = document.getElementById("colors-container");
+const seedColorEl = document.getElementById("seed-color-hex");
 
-renderSchemeNamesInOptions();
-
-function renderSchemeNamesInOptions() {
-  const schemeHtml = schemes
-    .map(
-      (scheme) =>
-        `<option value="${scheme}">${
-          scheme.charAt(0).toUpperCase() + scheme.slice(1)
-        }</option>`
-    )
-    .join("");
-  document.getElementById("select").innerHTML = schemeHtml;
-}
+colorsContainerEl.addEventListener("click", function (e) {
+  copyTextToClipboard(e);
+});
+seedColorEl.addEventListener("click", function (e) {
+  copyTextToClipboard(e);
+});
 
 document.getElementById("btn").addEventListener("click", () => {
   getQueryUrl();
@@ -32,7 +22,7 @@ function getQueryUrl() {
   const seedColor = document.querySelector("input").value.slice(1);
   const scheme = document.querySelector("select").value;
   const numColors = document.querySelector('input[type="radio"]:checked').id;
-  document.getElementById("seed-color-hex").textContent = seedColor;
+  seedColorEl.textContent = "#" + seedColor.toUpperCase();
   return `https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${scheme}&count=${numColors}`;
 }
 
@@ -48,7 +38,7 @@ function renderColorScheme(data) {
                 `
     )
     .join("");
-  document.getElementById("colors-container").innerHTML = colorHtml;
+  colorsContainerEl.innerHTML = colorHtml;
 }
 
 function copyTextToClipboard(e) {
@@ -59,7 +49,23 @@ function copyTextToClipboard(e) {
   if (e.target.classList.value.includes("color")) {
     copyText = e.target.id;
   }
-  console.log(copyText);
+  if (e.target.id === "seed-color-hex") {
+    copyText = e.target.textContent;
+  }
   navigator.clipboard.writeText(copyText);
   alert("Copied the text: " + copyText);
+}
+
+renderSchemeNamesInOptions();
+
+function renderSchemeNamesInOptions() {
+  const schemeHtml = schemes
+    .map(
+      (scheme) =>
+        `<option value="${scheme}">${
+          scheme.charAt(0).toUpperCase() + scheme.slice(1)
+        }</option>`
+    )
+    .join("");
+  document.getElementById("select").innerHTML = schemeHtml;
 }
